9f1ac904f0aedde405d2e257a2f3d89bbf8e4e8a
Shailen <shailen@gettruck.co.za>
2019-04-05 15:13:08 +0200

    Setup app shell

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,5 +5,51 @@
         <nuxt />
       </v-container>
     </v-content>
+    <v-bottom-nav
+      v-model="showBottomNav"
+      app
+      fixed
+      color="yellow"
+      :active.sync="page"
+    >
+      <v-btn
+        v-for="({ title, icon },i) in pages"
+        :key="i"
+        :value="title.toLowerCase()"
+        :to="{ name: title }"
+      >
+        <span>{{ title }}</span>
+        <v-icon>{{ icon }}</v-icon>
+      </v-btn>
+    </v-bottom-nav>
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showBottomNav: true,
+      page: 'dashboard',
+      pages: [
+        {
+          title: 'Dashboard',
+          icon: 'dashboard'
+        },
+        {
+          title: 'Courses',
+          icon: 'library_books'
+        },
+        {
+          title: 'Exams',
+          icon: 'assessment'
+        },
+        {
+          title: 'Account',
+          icon: 'account_circle'
+        }
+      ]
+    }
+  }
+}
+</script>
