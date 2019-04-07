feb675b66cecea5dffa18580c1942d3dc69ff77f
Shailen <shailen@gettruck.co.za>
2019-04-07 18:08:03 +0200

    Added some meta data and made more homepage changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <v-container fill-height>
     <v-layout justify-center align-center text-xs-center text-sm-center text-md-center>
-      <v-flex md6>
+      <v-flex md5>
+        <v-img src="/icon.png" class="mt-5 mb-5" height="200px" contain />
         <h1 class="display-1">
           K53 learners guide
         </h1>
-        <v-img src="/icon.png" class="mt-5 mb-5" height="200px" contain />
+        <p class="px-5 mt-3 body-2 font-weight-light">
+          An online K53 learners guide that looks modern and is fully accessible offline
+        </p>
         <v-btn color="yellow" :to="{ path: '/courses' }" round depressed>
           Check out app
         </v-btn>
@@ -16,6 +19,15 @@
 
 <script>
 export default {
-  layout: 'home'
+  layout: 'home',
+  head: {
+    title: 'K53 Learners Guide',
+    meta: [
+      {
+        name: 'description',
+        content: 'An online K53 learners guide that looks modern and is fully accessible offline'
+      }
+    ]
+  }
 }
 </script>
