5584a301b9ab2232e9a7ce2b2d35d1bf27fb825d
Shailen <shailen@gettruck.co.za>
2019-04-07 17:52:01 +0200

    Made a basic landing page

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fill-height>
+    <v-layout align-center text-xs-center>
+      <v-flex>
+        <h1 class="display-1">
+          K53 learners guide
+        </h1>
+        <img src="/icon.png" class="mt-5 mb-5" height="200px">
+        <v-btn color="yellow" :to="{ path: '/courses' }" round depressed>
+          Check out app
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'home'
+}
+</script>
