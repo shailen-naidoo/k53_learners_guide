2e8d882521d179abd7dbd78980c13cc2af718951
Shailen <shailen@gettruck.co.za>
2019-05-02 15:35:16 +0200

    Created a dashboard page

---
+++ pages/dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container
+    fill-height
+    grid-list-lg
+  >
+    <v-layout
+      row
+      wrap
+      justify-center
+      align-center
+    >
+      <v-flex md5 text-md-center>
+        <v-card flat color="grey lighten-5" class="border-radius">
+          <v-card-text class="title">
+            Don't worry, something cool will happen here pretty soon 😝
+          </v-card-text>
+          <v-img src="https://media0.giphy.com/media/l0HlKrB02QY0f1mbm/giphy.gif?cid=790b76115ccaf10250457771671b1a79&rid=giphy.gif" contain class="mt-5" style="border-radius: 10px;" />
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<style scoped>
+.border-radius {
+  border-radius: 5px;
+}
+
+.border-radius-btn {
+  border-radius: 2px;
+}
+</style>
