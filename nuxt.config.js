10fac3085b32b3b474681107a8fa2e1269a79598
Shailen <shailen@gettruck.co.za>
2019-05-15 20:02:52 +0200

    Modified linting rules

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,27 +8,40 @@ module.exports = {
   */
   head: {
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
     ],
     link: [
-      { rel: 'icon', type: 'png', href: '/icon.png' },
+      {
+        rel: 'icon',
+        type: 'png',
+        href: '/icon.png',
+      },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
-      }
-    ]
+        href: 'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700|Material+Icons',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css',
+      },
+    ],
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#fff', },
 
   /*
   ** Global CSS
   */
   css: [
-    '~/assets/style/app.styl'
+    '~/assets/style/app.styl',
   ],
 
   /*
@@ -36,7 +49,7 @@ module.exports = {
   */
   plugins: [
     '@/plugins/vuetify',
-    '@/plugins/staticData'
+    '@/plugins/staticData',
   ],
 
   /*
@@ -47,8 +60,8 @@ module.exports = {
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     ['@nuxtjs/google-analytics', {
-      id: 'UA-130574703-2'
-    }]
+      id: 'UA-130574703-2',
+    }, ],
   ],
   /*
   ** Axios module configuration
@@ -61,12 +74,14 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    transpile: ['vuetify/lib'],
-    plugins: [new VuetifyLoaderPlugin()],
+    transpile: ['vuetify/lib',],
+    plugins: [new VuetifyLoaderPlugin(),],
     loaders: {
       stylus: {
-        import: ['~assets/style/variables.styl']
-      }
+        import: [
+          '~assets/style/variables.styl',
+        ],
+      },
     },
     /*
     ** You can extend webpack config here
@@ -78,17 +93,19 @@ module.exports = {
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
         })
       }
-    }
+    },
   },
   manifest: {
     name: 'K53 Learners Guide',
     short_name: 'K53',
-    start_url: '/'
+    start_url: '/',
   },
   generate: {
-    routes: ['/courses/road-signs/topic/regulatory-signs']
-  }
+    routes: [
+      '/courses/road-signs/topic/regulatory-signs',
+    ],
+  },
 }
