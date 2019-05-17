34d136f13e2cf51580955b13da647dc8c5d3d0cf
Shailen <shailen@gettruck.co.za>
2019-05-17 22:11:44 +0200

    Added some markup for account

--- pages/account.vue
+++ pages/account.vue
@@ -9,19 +9,24 @@
       justify-center
       align-center
     >
-      <v-flex md5 text-md-center text-sm-center text-xs-center>
-        <v-card flat color="grey lighten-5" class="border-radius">
-          <v-card-text class="title">
-            Don't worry, something cool will happen here pretty soon 😝
-          </v-card-text>
-          <v-img src="https://media0.giphy.com/media/tXL4FHPSnVJ0A/giphy.gif?cid=790b76115ccaf10250457771671b1a79&rid=giphy.gif" class="mt-5" style="border-radius: 10px;" />
-        </v-card>
+      <v-flex md5 xs10 text-md-center text-sm-center text-xs-center>
+        <v-avatar size="80px" class="mb-3">
+          <img src="https://image.flaticon.com/icons/svg/1738/1738691.svg" alt="">
+        </v-avatar>
+        <p>Create an account to get access to some cool features</p>
+        <v-btn color="grey lighten-3" round small>
+          Create account
+        </v-btn>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+// import {
+//   auth,
+// } from '@/plugins/firebase'
+
 export default {
   head: {
     title: '🧑 Account | K53 Learner\'s Guide',
