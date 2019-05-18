8a0e040b13a7b25703d78d868d2dfeac9726cd28
Shailen <shailen@gettruck.co.za>
2019-05-18 10:51:43 +0200

    Setup auth for most major providers

--- pages/account.vue
+++ pages/account.vue
@@ -24,16 +24,30 @@
           Create an account to get access to some cool features
         </p>
         <v-btn color="grey lighten-3" round @click="showAccounts = true">
-          Create account
+          Login / Signup
         </v-btn>
+        <div class="text-md-center mt-5">
+          <p class="caption">
+            Interested in the people who made this project possible? <nuxt-link :to="{ name: 'courses' }">Find out more</nuxt-link> ❤
+          </p>
+        </div>
       </v-flex>
       <v-flex v-else md5 sm6>
         <no-ssr>
           <v-card class="border-radius">
             <v-card-title>
-              User account
+              <v-spacer />
+              <v-btn
+                small
+                depressed
+                outline
+                round
+                color="black"
+                @click="signOut"
+              >
+                Sign out
+              </v-btn>
             </v-card-title>
-            <v-divider />
             <v-card-text>
               <div class="text-md-center text-sm-center text-xs-center">
                 <v-avatar size="120px" class="mb-3 mt-3">
@@ -47,23 +61,27 @@
                 <v-subheader class="pl-0">
                   Settings
                 </v-subheader>
-                <v-checkbox label="Receive emails from our newsletter" class="pt-0" />
-                <v-checkbox label="Receive mobile/desktop notifications" class="pt-0 mt-0" />
+                <v-checkbox label="Receive email updates about the K53 Guide project" class="pt-0 mt-0" />
               </div>
             </v-card-text>
           </v-card>
+          <div class="text-md-center mt-5">
+            <p class="caption">
+              Want to help make this project even better? <nuxt-link :to="{ name: 'courses' }">Find out more</nuxt-link> ❤
+            </p>
+          </div>
         </no-ssr>
       </v-flex>
     </v-layout>
     <v-dialog v-model="showAccounts" max-width="500px">
       <v-card class="border-radius">
-        <v-card-title>Create K53 Guide account</v-card-title>
+        <v-card-title>Create / Login K53 Guide account</v-card-title>
         <v-divider />
         <v-card-text class="text-md-center text-sm-center text-xs-center pb-4">
           <v-btn fab depressed color="#DB4437" dark @click="signInWithGoogle">
             <v-icon>fab fa-google</v-icon>
           </v-btn>
-          <v-btn fab depressed color="#38A1F3" dark>
+          <v-btn fab depressed color="#38A1F3" dark @click="signInWithTwitter">
             <v-icon>fab fa-twitter</v-icon>
           </v-btn>
           <v-btn fab depressed color="#3C5A99" dark @click="signInWithFacebook">
@@ -85,6 +103,7 @@ import {
   auth,
   googleProvider,
   facebookProvider,
+  twitterProvider,
 } from '@/plugins/firebase.js'
 
 export default {
@@ -122,8 +141,6 @@ export default {
   },
   mounted() {
     auth.onAuthStateChanged((user) => {
-      // eslint-disable-next-line no-console
-      console.log(user)
       this.user = user
     })
   },
@@ -136,6 +153,14 @@ export default {
         this.showError = true
       }
     },
+    async signInWithTwitter() {
+      try {
+        await auth.signInWithPopup(twitterProvider)
+        this.showAccounts = false
+      } catch (e) {
+        this.showError = true
+      }
+    },
     async signInWithFacebook() {
       try {
         await auth.signInWithPopup(facebookProvider)
@@ -143,6 +168,9 @@ export default {
       } catch (e) {
         this.showError = true
       }
+    },
+    signOut() {
+      auth.signOut()
     },
   },
 }
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -19,3 +19,4 @@ if (!firebase.apps.length) {
 export const auth = firebase.auth()
 export const googleProvider = new firebase.auth.GoogleAuthProvider()
 export const facebookProvider = new firebase.auth.FacebookAuthProvider()
+export const twitterProvider = new firebase.auth.TwitterAuthProvider()
