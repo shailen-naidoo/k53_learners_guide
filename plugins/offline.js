544f446c92d962819dd81406200f30990662140b
Shailen <naidooshailen648@gmail.com>
2019-05-21 00:00:45 +0200

    Added offline init state logic

--- plugins/offline.js
+++ plugins/offline.js
@@ -2,6 +2,8 @@ export default ({ app, }) => {
   // eslint-disable-next-line no-console
   console.log('Offline/online pluging loaded');
 
+  app.store.commit('SET_OFFLINE', !window.navigator.onLine);
+
   window.addEventListener('offline', () => {
     app.store.commit('SET_OFFLINE', true);
   });
