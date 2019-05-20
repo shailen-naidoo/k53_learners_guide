2e525fdea8c41b67cfcaac8376d7aa09887d7d42
Shailen <naidooshailen648@gmail.com>
2019-05-20 22:40:11 +0200

    Fixed offline/online intial state

--- plugins/offline.js
+++ plugins/offline.js
@@ -1,6 +1,9 @@
 export default ({ app, }) => {
   // eslint-disable-next-line no-console
   console.info('Offline/online plugin loaded');
+
+  app.store.commit('SET_OFFLINE', !window.navigator.onLine);
+
   window.addEventListener('offline', (e) => {
     app.store.commit('SET_OFFLINE', true);
   });
