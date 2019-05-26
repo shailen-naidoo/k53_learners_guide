f33c8e1131f0e5ae6e0c006b1d2cf19ea1d71b65
Shailen <naidooshailen648@gmail.com>
2019-05-26 16:40:29 +0200

    Added comprehensive signs

--- json-generate.js
+++ json-generate.js
@@ -8,14 +8,14 @@ const getDirectories = path => fs.readdir(`${__dirname}/static/${path}`, 'utf-8'
 
 const createObject = files => files.map(file => ({
   name: '',
-  image: `/images/road-signs/regulatory-signs/reservation-signs/${file}`,
-  type: 'Reservation sign',
+  image: `/images/road-signs/regulatory-signs/comprehensive-signs/${file}`,
+  type: 'Comprehensive sign',
   description: '',
 }));
 
 const main = async () => {
-  const saveJSONToFile = data => fs.writeFile('./static/data/pages/courses/road-signs/topic/reservation.json', JSON.stringify(data, null, 2));
-  const roadSignsFolders = await getDirectories(`${ROAD_SIGNS_DIR}/regulatory-signs/reservation-signs`);
+  const saveJSONToFile = data => fs.writeFile('./static/data/pages/courses/road-signs/topic/comprehensive.json', JSON.stringify(data, null, 2));
+  const roadSignsFolders = await getDirectories(`${ROAD_SIGNS_DIR}/regulatory-signs/comprehensive-signs`);
   const JSONData = createObject(roadSignsFolders);
 
   await saveJSONToFile(JSONData);
