fff94c25de0610dd1b61fefa949020e1e5728ea2
shailen <shailen@lumkani.com>
2019-11-23 11:23:50 +0200

    Trying out axios

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -72,7 +72,10 @@ export default {
     ],
   },
   async asyncData({ $axios }) {
-    const { default: data } = await import('@/static/data/pages/courses/index.json');
+    // const { default: data } = await import('@/static/data/pages/courses/index.json');
+
+    // eslint-disable-next-line no-console
+    const { data } = await $axios.get('/data/pages/courses/index.json');
 
     return {
       ...data,
