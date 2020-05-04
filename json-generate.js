bb7f0f40d80e790c3f87a052fef2c9e951d80ac5
Shailen Naidoo <naidooshailen648@gmail.com>
2020-05-04 08:50:21 +0200

    Fixed linting errors

--- json-generate.js
+++ json-generate.js
@@ -10,22 +10,21 @@ const fs = require('fs-extra');
 const ROOT_DIR = 'images';
 const ROAD_SIGNS_DIR = `${ROOT_DIR}/road-signs`;
 
-
 const getDirectories = path => fs.readdir(`${__dirname}/static/${path}`, 'utf-8');
 
 const createObject = files => files.map(file => ({
-    name: '',
-    image: `/${ROAD_SIGNS_DIR}/${args.topic}/${args.subtopic}/${file}`,
-    type: `${args.subtopic}`,
-    description: '',
+  name: '',
+  image: `/${ROAD_SIGNS_DIR}/${args.topic}/${args.subtopic}/${file}`,
+  type: `${args.subtopic}`,
+  description: '',
 }));
 
 const main = async () => {
-    const saveJSONToFile = data => fs.writeFile(`./static/data/pages/courses/road-signs/topic/${args.subtopic}.json`, JSON.stringify(data, null, 2));
-    const roadSignsFolders = await getDirectories(`${ROAD_SIGNS_DIR}/${args.topic}/${args.subtopic}`);
-    const JSONData = createObject(roadSignsFolders);
+  const saveJSONToFile = data => fs.writeFile(`./static/data/pages/courses/road-signs/topic/${args.subtopic}.json`, JSON.stringify(data, null, 2));
+  const roadSignsFolders = await getDirectories(`${ROAD_SIGNS_DIR}/${args.topic}/${args.subtopic}`);
+  const JSONData = createObject(roadSignsFolders);
 
-    await saveJSONToFile(JSONData);
+  await saveJSONToFile(JSONData);
 };
 
 if (Object.keys(args).length) {
@@ -33,4 +32,3 @@ if (Object.keys(args).length) {
 } else {
   console.log('\nPlease pass in the args\n');
 }
-
